78955f96e2ef4115995fe97b26e52f9fbdebafff
Yusuf.Q <yusufqing920@gmail.com>
2019-12-29 09:44:18 +0800

    add some modules

---
+++ src/Repository/RepositoryItem/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { Mutation } from 'react-apollo';
+import Link from '../../Link';
+import '../style.css';
+
+const STAR_REPOSITORY = gql`
+  mutation($id: ID!) {
+    addStar(input: { starrableId: $id }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
+
+const RepositoryItem = ({
+  name,
+  url,
+  descriptionHTML,
+  primaryLanguage,
+  owner,
+  stargazers,
+  watchers,
+  viewerSubscription,
+  viewerHasStarred,
+}) => (
+  <div>
+    <div className="RepositoryItem-title">
+      <h2>
+        <Link href={url}>{name}</Link>
+      </h2>
+      <div className="RepositoryItem-title-action">
+        <Mutation mutation={STAR_REPOSITORY} variables={{ id }}>
+          {(addStar) => (
+            <Button
+              className={'RepositoryItem-title-action'}
+              onClick={addStar}
+            >
+              {stargazers.totalCount} Star
+            </Button>
+          )}
+        </Mutation>
+      </div>
+    </div>
+    <div className="RepositoryItem-description">
+      <div
+        className="RepositoryItem-description-info"
+        dangerouslySetInnerHTML={{ __html: descriptionHTML }}
+      />
+      <div className="RepositoryItem-description-details">
+        <div>
+          {primaryLanguage && (
+            <span>Language: {primaryLanguage.name}</span>
+          )}
+        </div>
+        <div>
+          {owner && (
+            <span>
+              Owner: <a href={owner.url}>{owner.login}</a>
+            </span>
+          )}
+        </div>
+      </div>
+    </div>
+  </div>
+);
+export default RepositoryItem;
